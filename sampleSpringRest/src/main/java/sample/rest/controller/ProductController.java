e1d2a21edd6cbbbbd2b2ed91bd0ab3f086ae83d0
cgpradeepkumar <cgpradeepkumar@gmail.com>
2018-01-05 20:01:28 +0530

    update product

--- sampleSpringRest/src/main/java/sample/rest/controller/ProductController.java
+++ sampleSpringRest/src/main/java/sample/rest/controller/ProductController.java
@@ -1,12 +1,11 @@
 package sample.rest.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sample.rest.services.ProductService;
 import sample.rest.services.entities.Product;
 
@@ -22,8 +21,20 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @ApiOperation(value = "View the list available product")
     @RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
     public Iterable<Product> list(Model model) {
         return productService.listAllProducts();
+    }
+
+    @ApiOperation(value = "Update a product")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, produces = "application/json")
+    public ResponseEntity updateProduct(@PathVariable Integer id, @RequestBody Product product) {
+        Product storedProduct = productService.getProductById(id);
+        storedProduct.setDescription(product.getDescription());
+        storedProduct.setImageUrl(product.getImageUrl());
+        storedProduct.setPrice(product.getPrice());
+        productService.saveProduct(storedProduct);
+        return new ResponseEntity("Product updated successfully", HttpStatus.OK);
     }
 }
--- sampleSpringRest/src/main/java/sample/rest/services/ProductServiceImpl.java
+++ sampleSpringRest/src/main/java/sample/rest/services/ProductServiceImpl.java
@@ -23,7 +23,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product getProductById(Integer id) {
-        return null;
+        return productRepository.findOne(id);
     }
 
     @Override
